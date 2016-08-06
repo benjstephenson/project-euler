594422ad4f1e648de372284af8988ba808354d16
Ben Stephenson <bjames.stephenson@gmail.com>
2016-08-06 13:45:00 +0100

    Problem 5 - smallest multiple.

--- src/main/scala/P5_EvenlyDivisible.scala
+++ src/main/scala/P5_EvenlyDivisible.scala
@@ -1,5 +1,7 @@
 package org.bste.euler
 
+import scala.annotation.tailrec
+
 object P5_EvenlyDivisible {
 
   /* 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
@@ -7,14 +9,21 @@ object P5_EvenlyDivisible {
    */
 
   def main(args: Array[String]) = {
-    def divides(x: Int): Boolean = {
-      val highestDivisor = (1 to 10).takeWhile(n => x % n == 0).toList
 
-      if (highestDivisor.last == 10) true else false
-    }
+    val result = check(1)
 
+    println(result)
+  }
 
-    println(divides(2521))
+  def divides(x: Int): Boolean = {
+    val highestDivisor = (1 to 20).takeWhile(n => x % n == 0).toList
+
+    if (highestDivisor.last == 20) true else false
+  }
+
+  @tailrec
+  def check(n: Int): Int = {
+    if (divides(n)) n else check(n+1)
   }
 
 }

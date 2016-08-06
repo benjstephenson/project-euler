db66b1b7be273af31c2523f519bed55cb7c24524
Ben Stephenson <bjames.stephenson@gmail.com>
2016-08-06 11:16:40 +0100

    LargestPrime, with help from the internet because my maths is rubbish.

--- src/main/scala/LargestPrime.scala
+++ src/main/scala/LargestPrime.scala
@@ -1,31 +1,15 @@
 package org.bste.euler
 
-import scala.annotation.tailrec
-
 object LargestPrime {
 
   def main(args: Array[String]) = {
 
-    val limit = 600851475143L
-    val res = (2L to 200L).map(getPrimes)
+    def factors(n: Long): List[Long] = (2 to math.sqrt(n).toInt)
+      .find(n % _ == 0).fold(List(n))(i => i.toLong :: factors(n / i))
 
-    println(s"res $res")
+    val r = factors(600851475143L).last
 
-  }
+    println(s"res $r")
 
-  def getPrimes(n: Long): Option[Long] = {
-    if (2 % n == 0) {
-      None
-    } else if (3 % n == 0) {
-      None
-    } else if (5 % n == 0) {
-      None
-    } else {
-      val root = Math.sqrt(n)
-
-      val range = (6 to root.toInt).forall(v => n % v == 0)
-
-      if (range) None else Some(n)
-    }
   }
 }

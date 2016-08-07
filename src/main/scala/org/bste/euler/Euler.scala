c227c69be0359424e0acab0041dc405375d31758
Ben Stephenson <bjames.stephenson@gmail.com>
2016-08-07 12:58:16 +0100

    Completed problem 6

--- src/main/scala/org/bste/euler/Euler.scala
+++ src/main/scala/org/bste/euler/Euler.scala
@@ -4,11 +4,12 @@ import org.bste.euler.problems._
 
 object Euler extends App {
 
-  Seq(new P1_MultiplesThreeAndFive,
-    new P2_EvenFibonacci,
-    new P3_LargestPrime,
-    new P4_PalindromeProduct,
-    new P5_EvenlyDivisible
+  Seq(//new P1_MultiplesThreeAndFive,
+    //new P2_EvenFibonacci,
+    //new P3_LargestPrime,
+    //new P4_PalindromeProduct,
+    //new P5_EvenlyDivisible,
+    new P6_SumOfSquares
   ) foreach( _.printResult )
 
 
--- src/main/scala/org/bste/euler/problems/P6_SumOfSquares.scala
+++ src/main/scala/org/bste/euler/problems/P6_SumOfSquares.scala
@@ -1,13 +1,25 @@
 package org.bste.euler.problems
 
-object P6_SumOfSquares {
+import org.bste.euler.Problem
 
-  /* The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
-   * The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025
-   * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum
-   * is 3025 − 385 = 2640.
-   *
-   * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-   */
+class P6_SumOfSquares(limit: Int = 100) extends Problem {
 
+  val title = "Problem 6 - Difference between the sum of the squares and the square of the sum"
+
+  val description = """|The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+  |The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025
+  |Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum
+  |is 3025 − 385 = 2640.
+  |Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.""".stripMargin
+
+  def answer = solve.toString
+
+
+  private def solve = {
+    val range = 1 to limit
+    val sumOfSquares = range.map(x => scala.math.pow(x, 2)).sum
+    val squareOfSum = scala.math.pow(range.sum, 2)
+
+    (squareOfSum - sumOfSquares).toLong
+  }
 }
